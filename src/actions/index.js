021d966bce2890735e4ea3c83b758e2ac7d0b7d4
fabijanski <mm.fabijanski@gmail.com>
2017-11-06 10:24:54 +0100

    Fix problem with http api request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = 'f36d9a0e165a41f150e553a7d30f7886';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
